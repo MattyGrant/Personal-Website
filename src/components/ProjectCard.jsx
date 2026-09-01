4bd7c3cb417eec7e226c7f226b673970f5690d61
Matthew <mrsenormatty@live.com>
2026-09-01 13:37:59 -0700

    card display modernization

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { GitHub } from "react-feather";
 const ProjectCard = (props) => {
     const {skills} = props;
     const skillsList = skills.map((skill) => (                                                                                                     // text-primaryBtn bg-primaryBg
-        <div className="inline-flex px-3 py-2 items-center justify-center flex-shrink-0 mr-2 rounded-xl text-primaryText bg-mediumOpaque text-md xl:text-lg font-light 
+        <div className="px-3 py-1.5 mx-0 my-auto rounded-xl text-primaryText bg-mediumOpaque text-md xl:text-lg font-light whitespace-nowrap
         shadow-darkOpaque shadow-lg" 
         key={skill.index}>
             {skill}
@@ -11,26 +11,27 @@ const ProjectCard = (props) => {
         )
     )
   return (
-    <div className=" mx-auto p-4 flex flex-col md:flex-row md:w-full md:justify-around md:my-4 md:text-left lg:p-8">
+    <div className="p-4 flex flex-col items-start bg-secondaryBg bg-opacity-50 rounded-md my-12 md:w-full md:justify-center md:text-left lg:my-0 lg:px-2 lg:py-0">
+        <h2 className="font-bold text-primaryBtn text-xl order-1 px-4 md:text-xl lg:py-4 2xl:text-2xl">{props.title}</h2>
+        <div className="flex flex-col order-3 md:order-2 md:my-auto md:p-2 lg:p-2 2xl:p-4">
+            <p className="text-primary font-light my-2 px-5 order-2 text-lg tracking-wide">{props.description}</p>
+            <div className=" flex flex-row-reverse w-full justify-between order-4 px-4 md:text-left my-3 2xl:my-4">
+                <div className="flex order-4">
+                    {props.repo && <a href={props.repo} className=" rounded-xl ml-4 p-2 md:ml-4 text-primaryText bg-accent hover:bg-secondaryBtn hover:text-accent  hover:shadow-md hover:cursor-pointer ">
+                        <GitHub className=""/> 
+                    </a>}
+                    
+                    <a href={props.url} className="p-2 self-start bg-secondaryBtn text-accent font-bold rounded-md hover:shadow-lg hover:cursor-pointer hover:bg-accent hover:text-secondaryBtn 
+                    md:px-2 md:py-2">Live Demo</a>
+                </div>
 
-        <div className="flex-col text-center md:flex-row md:text-left md:w-1/2 md:my-auto md:p-2 lg:p-2 2xl:p-4">
-                                    {/* text-primaryBtn */}
-            <h2 className="font-bold text-primaryBtn text-xl md:text-xl 2xl:text-2xl">{props.title}</h2>
-            <p className=" text-primary font-light my-2 sm:text-lg tracking-wide lg:w-4/5 lg:text-xl 2xl:w-2/3 2xl:text-2xl 2xl:my-4">{props.description}</p>
-            <div className="flex justify-center md:w-fit">
-                {skillsList}
-            </div>
-            <div className=" flex justify-center md:w-fit md:text-left my-3 2xl:my-4">
-                <a href={props.url} className=" p-2 bg-secondaryBtn text-accent font-bold rounded-lg hover:shadow-lg hover:cursor-pointer hover:bg-accent hover:text-secondaryBtn 
-                md:px-3 md:py-2">Live Demo</a>
-                {props.repo && <a href={props.repo} className=" rounded-xl ml-4 p-2 md:ml-4 text-primaryText bg-accent hover:bg-secondaryBtn hover:text-accent  hover:shadow-md hover:cursor-pointer ">
-                    <GitHub className=""/> 
-                </a>}
+                <div className="flex flex-wrap gap-3 order-4 w-1/2 md:w-2/3">
+                    {skillsList}
+                </div>
             </div>
 
         </div>
-        <img src={props.img} alt="" className="rounded-3xl object-fill w-3/4 mx-auto my-4 md:order-first md:w-1/2 md:p-2 md:my-2 md:mr-2 lg:w-[50%] 2xl:w-[45%]" />
-        <hr className="m-auto w-[95%] my-4 h-0.5 bg-primaryText bg-opacity-20 border-none md:hidden"/>
+        <img src={props.img} alt="" className="rounded-3xl object-fill w-[95%] mx-auto my-4 order-2 md:order-1 md:w-full md:px-4 md:my-6 md:mr-2 lg:w-full" />
     </div>
     
   )
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,19 +5,19 @@ const Projects = () => {
     const projectsList = projectData.map( project => ( <ProjectCard key={project.id} {...project} /> ))
     // const technologyList = technologyData.map( technology => ( <TechnologyCard key={technology.id} {...technology} /> ))
     return (
-        <section className="flex-col mx-auto my-8 mt-10 md:mt-12 md:w-[85%] lg:w-[75%] xl:w-2/3">
+        <section className="flex-col mx-auto my-8 mt-10 w-[95%] md:mt-12 md:w-[85%] lg:w-[70%]">
                                     {/* text-primaryText */}
-            <h1 className="text-4xl text-secondaryText mb-10 font-semibold md:p-10 md:mb-0 text-center " id="projects">Notable Work</h1>
+            <h1 className="text-4xl text-secondaryText my-10 font-semibold md:p-10 md:mb-0 text-center " id="projects">Notable Work</h1>
 
-            <div className="flex-col justify-between mx-auto">
+            <div className="flex flex-col w-[90%] mx-auto md:w-full lg:grid lg:grid-cols-2 lg:gap-8 xl:w-[95%]">
                 {projectsList}
             </div>
 
-            <h1 className="text-3xl text-secondaryText mb-10 font-semibold md:p-10 md:mb-0 text-center " id="projects">Technology Experience</h1>
-            <div className="flex flex-col items-start justify-center py-20 px-10 gap-4 rounded-xl bg-mediumOpaque">
-                <h3 className="text-2xl text-primaryText">Front-end Web Development</h3>
+            <h1 className="text-3xl text-secondaryText mt-20 mb-10 font-semibold md:p-10 md:mb-0 text-center " id="projects">Technology Experience</h1>
+            <div className="flex flex-col items-start justify-start w-[45%] mx-auto py-10 px-10 gap-4 rounded-xl bg-secondaryBg bg-opacity-60 xl:w-1/2">
+                <h3 className="text-2xl text-primaryText font-semibold">Web Development</h3>
                 <p className="text-lg text-slate-300">Multiple combined years of both personal and professional experience building applications using modern web development technologies:</p>
-                <div className="flex flex-row gap-4">
+                <div className="flex flex-row gap-4 flex-wrap">
                     <p className="bg-lightOpaque px-2 py-2 rounded-xl text-secondaryText shadow-sm">Javascript</p>
                     <p  className="bg-lightOpaque px-2 py-2 rounded-xl text-secondaryText shadow-sm">HTML/CSS</p>
                     <p  className="bg-lightOpaque px-2 py-2 rounded-xl text-secondaryText shadow-sm">React</p>
